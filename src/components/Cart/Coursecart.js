4b830121cec41ee7731ae3b1e749983738629e13
iyashjayesh <180303105046@paruluniversity.ac.in>
2020-11-16 01:28:51 +0530

    web address changed

--- src/components/Cart/Coursecart.js
+++ src/components/Cart/Coursecart.js
@@ -34,7 +34,7 @@ export default class Coursecart extends Component {
                                         data-item-id={node.id}
                                         data-item-name={node.title}
                                         data-item-price={node.price}
-                                        data-item-url=""
+                                        data-item-url="https://yash-react-course-app.netlify.app/"
                                         data-item-image={node.image.fixed.src}
                                         className="btn btn-warning snipcart-add-item">Join Now</button>
                                     </div>
